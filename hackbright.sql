c45d30e9d37cffad21620bfe4ed1d329890d7d99
Rosie <rosie@riveter13-md.swna.wdpr.disney.com>
2016-04-25 17:24:35 -0700

    Hackbright SQL for fun and profit.

---
+++ hackbright.sql
@@ -0,0 +1,167 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.1
+-- Dumped by pg_dump version 9.5.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: grades; Type: TABLE; Schema: public; Owner: rosie
+--
+
+CREATE TABLE grades (
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE grades OWNER TO rosie;
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: rosie
+--
+
+CREATE TABLE projects (
+    id integer NOT NULL,
+    title character varying(30),
+    description text,
+    max_grade integer
+);
+
+
+ALTER TABLE projects OWNER TO rosie;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: rosie
+--
+
+CREATE SEQUENCE projects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE projects_id_seq OWNER TO rosie;
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: rosie
+--
+
+ALTER SEQUENCE projects_id_seq OWNED BY projects.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: rosie
+--
+
+CREATE TABLE students (
+    first_name character varying(30),
+    last_name character varying(30),
+    github character varying(30)
+);
+
+
+ALTER TABLE students OWNER TO rosie;
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: rosie
+--
+
+ALTER TABLE ONLY projects ALTER COLUMN id SET DEFAULT nextval('projects_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: rosie
+--
+
+COPY grades (student_github, project_title, grade) FROM stdin;
+jhacks	Markov	10
+jhacks	Blockly	2
+sdevelops	Blockly	100
+sdevelops	Markov	50
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: rosie
+--
+
+COPY projects (id, title, description, max_grade) FROM stdin;
+1	Markov	Tweets generated from Markov chains	50
+2	Blockly	Programmatic Logic Puzzle Game	10
+3	Code Rosie	Code some stuff!	100
+4	Happy Stuff	Happy HAPPY things.	10
+5	Are you happy now?	We made more projects.  Take that.	10
+\.
+
+
+--
+-- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: rosie
+--
+
+SELECT pg_catalog.setval('projects_id_seq', 5, true);
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: rosie
+--
+
+COPY students (first_name, last_name, github) FROM stdin;
+Jane	Hacker	jhacks
+Sarah	Developer	sdevelops
+\.
+
+
+--
+-- Name: projects_pkey; Type: CONSTRAINT; Schema: public; Owner: rosie
+--
+
+ALTER TABLE ONLY projects
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
